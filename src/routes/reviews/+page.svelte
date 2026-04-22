e4380e7a05cf530465e2bca0ff6599af345a20e8
RedCatstone <114140180+RedCatstone@users.noreply.github.com>
2026-04-22 18:52:28 +0200

    more reviews

--- src/routes/reviews/+page.svelte
+++ src/routes/reviews/+page.svelte
@@ -31,6 +31,19 @@
 	</div>
 
 	<div class="review">
+		⭐⭐⭐⭐⭐ - William Shakespear<br />
+A thing most strange I did behold: a game
+Of noughts and crosses, once both calm and wise,
+Now drowned in noise, in lights that mock the mind.
+Each move a thunder, each small win a roar,
+As though great kings had fought upon this grid.
+The players rave in tongues of “EZ” and “LOL,”
+Their wits undone by flashing, frantic charm.
+O cursed board, thou trivial, gaudy jest—
+And yet… I played again.
+	</div>
+
+	<div class="review">
 		⭐⭐⭐⭐⭐ - Dieter Bohlen<br />Schmackhaft 👍
 	</div>
 
@@ -44,6 +57,25 @@
 
 	<div class="review">
 		⭐⭐⭐⭐⭐ - Bob the builder<br />solid constructionwork 🏗
+	</div>
+	
+	<div class="review">
+		⭐⭐⭐⭐⭐ - Otto von Bismarck<br />Deutschland
+	</div>
+
+	
+	<div class="review">
+		⭐⭐⭐⭐⭐ - Martin<br />
+You shall have no other gods. (Do not worship, love, or trust anything above God).
+You shall not misuse the name of the Lord your God. (Do not use God’s name to curse or deceive; use it to praise and pray).
+Remember the Sabbath day by keeping it holy. (Do not despise preaching and God’s Word, but hold it sacred).
+Honor your father and your mother. (Do not despise or anger parents, but respect and love them).
+You shall not murder. (Do not harm or threaten neighbors, but help them).
+You shall not commit adultery. (Live in a sexually pure and decent way).
+You shall not steal. (Do not take money or property, but help others improve their living).
+You shall not give false testimony against your neighbor. (Do not lie or ruin a neighbor’s reputation, but defend them).
+You shall not covet your neighbor’s house. (Do not try to get a neighbor’s home/property unfairly).
+You shall not covet your neighbor’s wife, or his manservant or maidservant, his ox or donkey, or anything that belongs to your neighbor. (Do not desire to take, force away, or entice away what belongs to a
 	</div>
 </div>
 
